1685bd2731967ad380f796442e345d4b1cdc0478
Folded Lettuce <45380013+FoldedLettuce@users.noreply.github.com>
2022-10-15 17:05:35 -0500

    Edited Setup

--- src/pages/setup.tsx
+++ src/pages/setup.tsx
@@ -38,39 +38,29 @@ const Index = ({ stats, topRepos }: AppProps) => {
             transition={{ ease: "easeOut", duration: 0.15 }}
             className="mt-24 w-full mb-32"
         >
-            <h1 className="mt-36 font-bold text-4xl md:text-5xl mb-4">Hey, I'm Lettuce 👋</h1>
-            <p className="text-gray-800 dark:text-gray-300 leading-6 tracking-wide mb-12">
-                I'm a Developer, Customer Support Representative, Proxy Company Manager, and Tech enthusiast from the United States.
-            </p>
+            <h1 className="mt-36 font-bold text-4xl md:text-5xl mb-4">Hey, check my setups out!</h1>
 
-            <h2 className="font-medium text-3xl mb-4">What I Do 💭</h2>
+            <h2 className="font-medium text-3xl mb-4">Desk</h2>
             <p className="text-gray-800 dark:text-gray-300 leading-6 font-light tracking-wide mb-12">
-                I'm passionate about everything technology. Whether it's Cybersecurity, Programming, Networking, and so much
-                more I do it all. I strive to learn more about all of these topics day, and utilize my knowledge to further
-                understand <i>how</i> or <i>why</i> the world functions.
+
             </p>
 
-            <h2 className="font-medium text-3xl mb-4">Technologies 💻</h2>
+            <h2 className="font-medium text-3xl mb-4">Home Lab</h2>
+            <image src="https://i.imgur.com/h5fMTge.jpg"></image>
             <p className="text-gray-800 dark:text-gray-300 leading-6 font-light tracking-wide mb-6">
-                I use a variety of tools to streamline my development process and increase the quality of both my code,
-                and my projects. Below is a list of technologies and languages I've had experience with in the past, or
-                use currently.
-            </p>
+                Equipment:
+                - 24 Port Cable Master Keystone Patch Panel
+                - Unifi Switch 24 PoE 
+                - Unifi Dream Machine Pro
+                - APC 1200VA UPS
+                - StarTech 6U Network Rack
+                - 3x Unifi U6 Lite Access Points
 
-            <h2 className="font-medium text-3xl mb-4">Projects 🛠️</h2>
-            <p className="text-gray-800 dark:text-gray-300 leading-6 font-light tracking-wide mb-6">
-                In my free time, I enjoy creating open source projects on{" "}
-                <a
-                    href="https://github.com/foldedlettuce"
-                    rel="noreferrer"
-                    className="font-semibold text-violet-500 hover:underline"
-                >
-                    GitHub
-                </a>
-                , so I can learn from others and showcase what I know. In total, all of my open sourced projects have
-                earnt me <span className="font-bold text-black dark:text-slate-200">{stats.stars}</span> stars on
-                GitHub, and <span className="font-bold text-black dark:text-slate-200">{stats.forks}</span> forks. Below
-                are some of my most popular repositories.
+                Cabling:
+                - Monoprice Cat6A Patch Cables
+                - trueCABLE Cat6 Outdoor｜Shielded - Black
+                - trueCABLE Cat6 Riser｜Unshielded - White
+                - trueCABLE Cat6 Punch Down Keystone Jack - White
             </p>
         </motion.div>
     );
